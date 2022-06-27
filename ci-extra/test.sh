dd717435ae1556c1494bdcab56ac0851c596eb4d
Denis Vorkozhokov <30625073+lejabque@users.noreply.github.com>
2022-06-27 19:12:07 +0300

    Update test.sh

--- ci-extra/test.sh
+++ ci-extra/test.sh
@@ -2,4 +2,17 @@
 set -euo pipefail
 IFS=$' \t\n'
 
-cmake-build-$1/tests
+gdb -q --batch \
+    -ex 'handle SIGHUP nostop pass' \
+    -ex 'handle SIGQUIT nostop pass' \
+    -ex 'handle SIGPIPE nostop pass' \
+    -ex 'handle SIGALRM nostop pass' \
+    -ex 'handle SIGTERM nostop pass' \
+    -ex 'handle SIGUSR1 nostop pass' \
+    -ex 'handle SIGUSR2 nostop pass' \
+    -ex 'handle SIGCHLD nostop pass' \
+    -ex 'set style enabled on' \
+    -ex 'set print frame-arguments all' \
+    -ex 'run' \
+    -ex 'thread apply all bt -frame-info source-and-location -full' \
+    --args cmake-build-$1/tests
